c84515a96c9020d5f3b6b3baa6596ee1e490746b
ifokzm <ifokzm@163.com>
2018-01-06 09:03:08 +0800

    update

--- src/main/java/server/Application.java
+++ src/main/java/server/Application.java
@@ -12,59 +12,10 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan(value = {"controller", "service", "cache", "nio", "swagger"})
 public class Application
 {
-    
-    // @ConfigurationProperties(prefix = "spring.datasource")
-    // public DataSource dataSource()
-    // {
-    // return new com.alibaba.druid.pool.DruidDataSource();
-    // }
-    //
-    // @Bean
-    // public SqlSessionFactory sqlSessionFactoryBean()
-    // throws Exception
-    // {
-    // SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
-    // sqlSessionFactoryBean.setDataSource(dataSource());
-    // sqlSessionFactoryBean.setTypeAliasesPackage("pojo");
-    //
-    // PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
-    // sqlSessionFactoryBean.setMapperLocations(resolver.getResources("classpath:/mybatis/*.xml"));
-    // return sqlSessionFactoryBean.getObject();
-    // }
-    //
-    // @Bean
-    // public PlatformTransactionManager transactionManager()
-    // {
-    // return new DataSourceTransactionManager(dataSource());
-    // }
-    //
-    // @Bean
-    // public ServletRegistrationBean dispatcherRegistration(DispatcherServlet dispatcherServlet)
-    // {
-    // ServletRegistrationBean registration = new ServletRegistrationBean(dispatcherServlet);
-    // // registration.getUrlMappings().clear();
-    // // registration.addUrlMappings("*.do");
-    // // registration.addUrlMappings("*.json");
-    //
-    // registration.setServlet(new StatViewServlet());
-    // registration.addUrlMappings("/druid/*");
-    // return registration;
-    // }
-    //
-    // @Bean
-    // public FilterRegistrationBean filterRegistrationBean()
-    // {
-    // FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
-    // filterRegistrationBean.setFilter(new WebStatFilter());
-    // filterRegistrationBean.addUrlPatterns("/*");
-    // filterRegistrationBean.addInitParameter("exclusions", "*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid/*");
-    // return filterRegistrationBean;
-    // }
     public static void main(String[] args)
         throws Exception
     {
         System.setProperty("spring.devtools.restart.enabled", "true");
         SpringApplication.run(Application.class, args);
     }
-    
 }
